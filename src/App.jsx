09f4eedf09d6fc62b46061fa22bf9d5885a9b1c8
rupesh-dev30 <cgcrupesh@gmail.com>
2025-07-27 21:45:23 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="text-4xl font-bold">HLO</div>;
+  return <div className="text-4xl font-bold"><Navbar /></div>;
 };
 
 export default App;
