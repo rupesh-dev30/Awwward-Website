aa52798c2ad17f8885ae9bc0c56b43c14cf8bb4f
rupesh-dev30 <cgcrupesh@gmail.com>
2025-07-27 22:34:26 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-  return <div className="text-4xl font-bold"><Navbar /></div>;
+  return (
+    <div className="text-4xl font-bold">
+      <Navbar />
+      <HeroSection />
+      <div className="h-dvh border"></div>
+    </div>
+  );
 };
 
 export default App;
